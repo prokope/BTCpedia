d0385d9b1e8c805b3e06bf7ebb0d0b39543e32f7
Paulo Procopio <paulorprocopio@gmail.com>
2025-08-18 11:09:35 -0300

    Add loop to update API each hour

--- charts.py
+++ charts.py
@@ -1,24 +1,35 @@
-import pandas as pd
 import plotly.graph_objects as go
-from request import btc_last_7_days_price, btc_last_month_price
+from request import btc_last_7_days_price, btc_last_2_months_price, btc_last_year_price
+from time import sleep
 
-# BTC Last 7 days Chart
-print(btc_last_7_days_price)
-btc_last_7_days_chart = go.Figure([go.Scatter(x=btc_last_7_days_price.index.to_list(), y=btc_last_7_days_price)])
-btc_last_7_days_chart.update_traces(line_color="#ff6d4d")
-btc_last_7_days_chart.update_layout(
-    plot_bgcolor='rgba(0,0,0,0)',
-    paper_bgcolor='#e6e6e6'
-)
-# btc_last_7_days_chart.show()
-btc_last_7_days_chart.write_html("btc_7_days_chart.html")
+while True:
+    # BTC Last 7 days Chart
+    print(btc_last_7_days_price)
+    btc_last_7_days_chart = go.Figure([go.Scatter(x=btc_last_7_days_price.index.to_list(), y=btc_last_7_days_price)])
+    btc_last_7_days_chart.update_traces(line_color="#ff6d4d")
+    btc_last_7_days_chart.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='#e6e6e6'
+    )
+    # btc_last_7_days_chart.show()
+    btc_last_7_days_chart.write_html("btc_7_days_chart.html")
 
-# BTC Last Month Chart
-btc_last_month_chart = go.Figure([go.Scatter(x = btc_last_month_price.index.to_list(), y=btc_last_month_price)])
-btc_last_month_chart.update_layout(
-    plot_bgcolor='rgba(0,0,0,0)',
-    paper_bgcolor='#e6e6e6'
-)
-btc_last_month_chart.update_traces(line_color="#ff6d4d")
-btc_last_month_chart.show()
-btc_last_month_chart.write_html("btc_last_month_chart.html")
+    # BTC Last Month Chart
+    btc_last_2_months_chart = go.Figure([go.Scatter(x = btc_last_2_months_price.index.to_list(), y=btc_last_2_months_price)])
+    btc_last_2_months_chart.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='#e6e6e6'
+    )
+    btc_last_2_months_chart.update_traces(line_color="#ff6d4d")
+    btc_last_2_months_chart.write_html("btc_last_2_months_chart.html")
+
+    # BTC Last Year Price
+    btc_last_year_chart = go.Figure([go.Scatter(x=btc_last_year_price.index.to_list(), y=btc_last_year_price)])
+    btc_last_year_chart.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='#e6e6e6'
+    )
+    btc_last_year_chart.update_traces(line_color="#ff6d4d")
+    btc_last_year_chart.show()
+    btc_last_year_chart.write_html("btc_Last_year_chart.html")
+    sleep(3600)
--- script.js
+++ script.js
@@ -1,15 +1,35 @@
+function body_transition()
+{
+    document.body.style.opacity = 1;
+}
+
 function chart_changed()
 {
     const menu = document.getElementById('chart_options');
     const iframe = document.getElementById('iframe_chart');
 
-    if (menu.value === "btc_last_month_chart")
-    {
-        iframe.src = "btc_last_month_chart.html";
-    }
+    iframe.classList.add('fade-out');
 
-    else if (menu.value === "btc_7_days_chart")
+    setTimeout (() =>
     {
-        iframe.src = "btc_7_days_chart.html";
-    }
+        if (menu.value === "btc_last_2_months_chart")
+        {
+            iframe.src = "btc_last_2_months_chart.html";
+        }
+
+        else if (menu.value === "btc_7_days_chart")
+        {
+            iframe.src = "btc_7_days_chart.html";
+        }
+
+        else if (menu.value == "btc_last_year_chart")
+        {
+            iframe.src = "btc_last_year_chart.html";
+        }
+
+        iframe.onload = () =>
+        {
+            iframe.classList.remove("fade-out");
+        }
+    }, 350);
 }
